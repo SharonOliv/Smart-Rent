59ba36b32bdeaf3611c5fbc3f42c75d32a59544b
Sharon Olivia <sharonoliviachintala@gmail.com>
2026-09-18 12:36:03 +0530

    Resend api key

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -3,16 +3,16 @@
 //But companies use transactional email apis like Sendgrid, Mailgun, Postmark, Resend etc. to send emails
 //because sender domain has better reputation and ipv6 will not reject the request, just like it rejected the smpt port 587 request from nodemailer. So we use Resend here to send emails.
 
-const { Resend } = require("resend");
+// utils/mailer.js
+import { Resend } from "resend";
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-async function sendVerificationEmail(toEmail, verificationLink) {
+export async function sendVerificationEmail(toEmail, verificationLink) {
   await resend.emails.send({
-    from: "Smart Rent <onboarding@resend.dev>", // or your verified domain address
+    from: "Smart Rent <onboarding@resend.dev>",
     to: toEmail,
     subject: "Verify your email",
     html: `<p>Click <a href="${verificationLink}">here</a> to verify your account.</p>`,
   });
 }
-
-module.exports = { sendVerificationEmail };
